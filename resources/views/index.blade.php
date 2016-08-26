a2b47761bcef8fcd47840423b38ec029cc7f6ed4
Ricardo Marques <ricardo.ds.marques@gmail.com>
2016-08-26 14:55:21 +0000

    added app icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,9 @@
     <!--Import Google Icon Font-->
     <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
+    <!-- App Icon -->
+    <link rel="icon" href="/app/assets/novasbe_logo.png">
+
     <meta charset="utf-8">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
