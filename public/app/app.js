7312a090e12d9517e57ebad44f42f6eba7d8b20a
Ricardo Marques <ricardo.ds.marques@gmail.com>
2016-08-28 13:10:29 +0000

    fixed expired token bug

--- public/app/app.js
+++ public/app/app.js
@@ -48,21 +48,21 @@ angular.module('checkInManager', [
         $urlRouterProvider.otherwise('/events');
 
         $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function ($q, $location, $localStorage) {
-           return {
-               'request': function (config) {
-                   config.headers = config.headers || {};
-                   if ($localStorage.token) {
-                       config.headers.Authorization = 'Bearer ' + $localStorage.token;
-                   }
-                   return config;
-               },
-               'responseError': function (response) {
-                   if (response.status === 400 || response.status === 403) {
-                       $location.path('/signin');
-                   }
-                   return $q.reject(response);
-               }
-           };
+            return {
+                'request': function (config) {
+                    config.headers = config.headers || {};
+                    if ($localStorage.token) {
+                        config.headers.Authorization = 'Bearer ' + $localStorage.token;
+                    }
+                    return config;
+                },
+                'responseError': function (response) {
+                    if (response.status === 400 || response.status === 401 || response.status === 403) {
+                        $location.path('/signin');
+                    }
+                    return $q.reject(response);
+                }
+            };
         }]);
     })
 
