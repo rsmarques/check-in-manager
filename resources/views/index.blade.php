d3357d530bb42c0aa8b89c4af3c65291e2a53231
Ricardo Marques <ricardo.ds.marques@gmail.com>
2017-01-11 00:56:06 +0000

    frotend/css refactor

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <link rel='stylesheet prefetch' href="//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic&subset=latin,cyrillic">
 
     <!-- App Icon -->
-    <link rel="icon" href="/app/assets/novasbe_logo.png">
+    <link rel="icon" href="/images/logo/novasbe_logo.png">
     <link rel="apple-touch-icon" href="/app/assets/novasbe_logo.png">
 
     <meta charset="utf-8">
