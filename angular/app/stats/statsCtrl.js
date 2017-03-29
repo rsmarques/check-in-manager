708ffc4fc06c2bf1c45fe6b6f33c87dead87f244
Ricardo Marques <ricardo.ds.marques@gmail.com>
2017-03-29 19:46:27 +0000

    added more stats graphs

--- angular/app/stats/statsCtrl.js
+++ angular/app/stats/statsCtrl.js
@@ -30,10 +30,11 @@
                 stacked: true,
                 xAxis: {
                     axisLabel: 'Industry',
+                    rotateLabels: -45,
                     showMaxMin: false
                 },
                 yAxis: {
-                    axisLabel: 'Y Axis',
+                    axisLabel: 'Attendance',
                     axisLabelDistance: -20,
                     tickFormat: function(d){
                         return d3.format(',.1f')(d);
@@ -244,6 +245,117 @@
             {
                 "key":'Leadership',
                 "y":0.1247761751552973
+            }
+        ];
+
+        $scope.options.time = {
+            chart: {
+                type: 'lineChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 40,
+                    left: 55
+                },
+                x: function(d){ console.log(d); return d.x; },
+                y: function(d){ console.log(d); return d.y; },
+                useInteractiveGuideline: true,
+                xAxis: {
+                    axisLabel: 'Date',
+                    // rotateLabels: -45,
+                    tickFormat: function(d){
+                        return d3.time.format('%B %Y')(new Date(d))
+                    }
+                },
+                yAxis: {
+                    axisLabel: 'Attendance',
+                    axisLabelDistance: -10
+                }
+            }
+        };
+
+        $scope.data.time = [
+            {
+                values: [{"x": 1472688000000, "y": 400}, {"x": 1475280000000, "y": 300}, {"x": 1477958400000, "y": 128}, {"x": 1480550400000, "y": 333}, {"x": 1485907200000, "y": 520}, {"x": 1488326400000, "y": 400}],      //values - represents the array of {x,y} data points
+                key: 'Masters', //key  - the name of the series.
+                color: '#ff7f0e',  //color - optional: choose your own line color.
+                strokeWidth: 2,
+                area: true      //area - set to true if you want this line to turn into a filled area chart.
+            },
+            {
+                values: [{"x": 1472688000000, "y": 132}, {"x": 1475280000000, "y": 221}, {"x": 1477958400000, "y": 60}, {"x": 1480550400000, "y": 120}, {"x": 1485907200000, "y": 400}, {"x": 1488326400000, "y": 200}],      //values - represents the array of {x,y} data points
+                key: 'Bachelor',
+                color: '#2ca02c',
+                area: true      //area - set to true if you want this line to turn into a filled area chart.
+            },
+            {
+                values: [{"x": 1472688000000, "y": 32}, {"x": 1475280000000, "y": 21}, {"x": 1477958400000, "y": 10}, {"x": 1480550400000, "y": 12}, {"x": 1485907200000, "y": 40}, {"x": 1488326400000, "y": 20}],      //values - represents the array of {x,y} data points
+                key: 'Other',
+                color: '#7777ff',
+                area: true      //area - set to true if you want this line to turn into a filled area chart.
+            }
+        ];
+
+        $scope.options.countries = {
+            chart: {
+                type: 'pieChart',
+                height: 320,
+                x: function(d){return d.key;},
+                y: function(d){return d.y;},
+                showLabels: true,
+                labelSunbeamLayout: false,
+                donut: false,
+                labelType: "percent",
+                duration: 500,
+                labelThreshold: 0.01,
+                legend: {
+                    margin: {
+                        top: 5,
+                        right: 35,
+                        bottom: 5,
+                        left: 0
+                    }
+                }
+            }
+        };
+
+        $scope.data.countries = [
+            {
+                "key":'Portugal',
+                "y":0.8387871281835065
+            },
+            {
+                "key":'Germany',
+                "y":0.28212354479384876
+            },
+            {
+                "key":'Brasil',
+                "y":0.16845584598910426
+            },
+            {
+                "key":'China',
+                "y":0.17614274374453373
+            },
+            {
+                "key":'France',
+                "y":0.15998712084908245
+            },
+            {
+                "key":'Thailand',
+                "y":0.15676179440625723
+            },
+            {
+                "key":'Spain',
+                "y":0.1746058890227924
+            },
+            {
+                "key":'Great Britain',
+                "y":0.17364961027332312
+            },
+            {
+                "key":'Other',
+                "y":0.35354579937776675
             }
         ];
 
